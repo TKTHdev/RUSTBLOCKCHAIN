9ba433d632469ce236e98ea498cccdca7fd73749
高田晴成 <tkt0430@keio.jp>
2023-01-14 16:54:50 +0900

    ADDED DETAILS IN TRANSACTION.rs

---
+++ src/TRANSACTION.rs
@@ -0,0 +1,12 @@
+pub mod t
+{
+    use std::time::{SystemTime,UNIX_EPOCH,Duration};
+
+    pub struct Transaction
+    {
+        pub amount:u32,
+        pub sender_key:String,
+        pub receiver_key:String,
+        pub timestamp:Duration,
+    }
+}
